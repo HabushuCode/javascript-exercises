724acddd00111ead1862f7a9d5dba62a411b316c
Crystal <110324298+HabushuCode@users.noreply.github.com>
2023-12-20 20:56:32 +0700

    sumAll exercise is completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+   if(start < 0 || end < 0) {
+    return 'ERROR';
+   } else if (!Number.isInteger(start)|| !Number.isInteger(end)){
+    return 'ERROR';
+   } else if (start>end) {
+    let temp = start;
+    start = end;
+    end =temp;
+   }
+   
+    let total = 0;
+    for(let i=start; i<=end; i++){
+        total+=i;
+    }
+    return total;
 };
 
 // Do not edit below this line
