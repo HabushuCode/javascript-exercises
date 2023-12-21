8b949a7a23691f257d2d91bf4379490da062282e
Crystal <110324298+HabushuCode@users.noreply.github.com>
2023-12-21 22:29:26 +0700

    tempConversion exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  //F to C is (x-32)* 5/9 
+  return Math.round((((Fahrenheit-32)*5/9)*10))/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+    // C to F x*9/5+32 
+  return Math.round(((Celsius*9/5+32)*10))/10;
 };
 
 // Do not edit below this line
